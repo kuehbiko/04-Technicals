a45a30afcebc08a1eaa9f42b5c1c054a31d835e3
e. lim <88494428+kuehbiko@users.noreply.github.com>
2023-09-07 19:52:35 +0800

    Add files via upload

---
+++ SQL/LeetCode/SQL Study Plan I/06-UNION/197 - Rising Temperature.sql
@@ -0,0 +1,24 @@
+-- 197. Rising Temperature
+/*
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+*/
+
+SELECT w1.id
+FROM weather w1, weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 
+ AND w1.temperature > w2.temperature;
